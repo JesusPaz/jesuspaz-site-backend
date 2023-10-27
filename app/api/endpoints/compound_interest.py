e5ca48cfb7e8e6cebede77c6a5c0f942f537ad8a
Jesus Paz <jesus@nodesource.com>
2023-10-26 21:21:44 -0500

    return interest table (#2)
    * return interest table
    
    * fix Yearly_Interest

--- app/api/endpoints/compound_interest.py
+++ app/api/endpoints/compound_interest.py
@@ -1,16 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException
-from app.schemas.compound_interest import CompoundInterestInput, CompoundInterestOutput
+from app.schemas.compound_interest import CompoundInterestInput, CompoundInterestYearDetail
 from app.services import compound_interest_service
+from typing import List
 
 router = APIRouter()
 
 
-@router.post("/compound-interest/", response_model=CompoundInterestOutput)
+@router.post("/compound-interest/", response_model=List[CompoundInterestYearDetail])
 async def calculate_compound_interest(
     data: CompoundInterestInput
-) -> CompoundInterestOutput:
+) -> List[CompoundInterestYearDetail]:
+
     """
     Calculate compound interest based on provided input.
     """
-    result = compound_interest_service.calculate_compound_interest(data)
+    result = compound_interest_service.calculate_compound_interest_details(
+        data)
     return result
--- app/services/compound_interest_service.py
+++ app/services/compound_interest_service.py
@@ -1,26 +1,42 @@
-from app.schemas.compound_interest import CompoundInterestInput, CompoundInterestOutput
+from app.schemas.compound_interest import CompoundInterestInput, CompoundInterestOutput, CompoundInterestYearDetail
 
 
-def calculate_compound_interest(data: CompoundInterestInput) -> CompoundInterestOutput:
-    """
-    Calculate the compound interest based on the given data.
-
-    Parameters:
-    - data (CompoundInterestInput): The input data including principal, rate, time, and frequency.
-
-    Returns:
-    - CompoundInterestOutput: The calculated total amount and interest amount.
-    """
-    # Extract the data
+def calculate_compound_interest_details(data: CompoundInterestInput) -> CompoundInterestOutput:
     principal = data.principal
-    rate = data.rate / 100  # Convert the rate from percentage to a fraction
-    time = data.time
+    rate = data.rate / 100
+    time = int(data.time)
     frequency = data.frequency
+    contribution = data.contribution * \
+        data.contribution_frequency  # total contribution per year
 
-    # Compound interest formula: A = P(1 + r/n)^(nt)
-    total_amount = principal * (1 + rate / frequency) ** (frequency * time)
+    results = []
 
-    # Calculate the interest earned
-    interest_amount = total_amount - principal
+    for year in range(1, time + 1):
+        initial_balance_for_year = principal
 
-    return CompoundInterestOutput(total_amount=total_amount, interest_amount=interest_amount)
+        # Compound interest for the principal
+        principal = principal * (1 + rate/frequency)**frequency
+
+        # Future value of the contributions for this year
+        contributions_fv = contribution * \
+            ((1 + rate/frequency)**frequency - 1) * (frequency/rate)
+
+        # Update principal with the contributions
+        principal += contributions_fv
+
+        yearly_interest = principal - initial_balance_for_year - contribution
+
+        # Append the details for this year to the results
+        results.append(CompoundInterestYearDetail(
+            Year=year,
+            # remove this year contributions for initial balance
+            Initial_Balance=principal - contributions_fv,
+            Yearly_Contributions=contribution,
+            Total_Contributions=contribution * year,  # total contributions until this year
+            # total - initial balance - this year contribution
+            Yearly_Interest=yearly_interest,
+            Total_Interest=principal - data.principal - contribution * year,
+            Final_Balance=principal
+        ))
+
+    return results
