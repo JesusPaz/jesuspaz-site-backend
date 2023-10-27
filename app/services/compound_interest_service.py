272bcc04c1c27bfefe2d14b39aa4a600bafa8c8a
Jesus Hernando Paz Bolaños <jesus@nodesource.com>
2023-10-26 21:20:55 -0500

    fix Yearly_Interest

--- app/services/compound_interest_service.py
+++ app/services/compound_interest_service.py
@@ -12,6 +12,8 @@ def calculate_compound_interest_details(data: CompoundInterestInput) -> Compound
     results = []
 
     for year in range(1, time + 1):
+        initial_balance_for_year = principal
+
         # Compound interest for the principal
         principal = principal * (1 + rate/frequency)**frequency
 
@@ -22,6 +24,8 @@ def calculate_compound_interest_details(data: CompoundInterestInput) -> Compound
         # Update principal with the contributions
         principal += contributions_fv
 
+        yearly_interest = principal - initial_balance_for_year - contribution
+
         # Append the details for this year to the results
         results.append(CompoundInterestYearDetail(
             Year=year,
@@ -30,10 +34,8 @@ def calculate_compound_interest_details(data: CompoundInterestInput) -> Compound
             Yearly_Contributions=contribution,
             Total_Contributions=contribution * year,  # total contributions until this year
             # total - initial balance - this year contribution
-            Yearly_Interest=principal - \
-            (principal - contributions_fv) - contribution,
-            Total_Interest=principal - data.principal - contribution * \
-            year,  # total - initial principal - total contributions
+            Yearly_Interest=yearly_interest,
+            Total_Interest=principal - data.principal - contribution * year,
             Final_Balance=principal
         ))
 
